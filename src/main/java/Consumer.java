f088e85f6428177433e45ec06eb2973ece7fa65a
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-02-10 14:42:09 -0500

    L89 - REST API Client - POST request to https://reqres.in/api/users

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -1,4 +1,8 @@
+import org.springframework.http.HttpEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Consumer {
     public static void main(String[] args) {
@@ -6,8 +10,14 @@ public class Consumer {
 
         RestTemplate restTemplate = new RestTemplate();
 
-        String url = "https://reqres.in/api/users/2";
-        String response = restTemplate.getForObject(url, String.class);
+        Map<String, String> jsonToSend = new HashMap<>();
+        jsonToSend.put("name", "Test name");
+        jsonToSend.put("job", "test job");
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(jsonToSend);
+
+        String url = "https://reqres.in/api/users";
+        String response = restTemplate.postForObject(url, request, String.class);
 
         System.out.println(response);
     }
